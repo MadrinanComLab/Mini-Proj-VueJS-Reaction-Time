a05c54c444ee8395129a1c3f93e201fb23046bc0
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-22 20:44:02 +0700

    Vue.js Lifecycle hooks was discussed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
+<!--/ 
+  THE NET NINJA DISCUSSED ABOUT THE LIFECYCLE HOOKS,  HERE IS THE DOCUMENTATION FROM VUE:
+  https://vuejs.org/guide/essentials/lifecycle.html#lifecycle-diagram
+/-->
+
 <template>
   <h1>MadrinanComLab Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button><!--/ THIS BUTTON WILL BE DISABLED IF THE USER STARTED THE GAME... /-->
+
+  <!--/ BLOCK COMPONENT WILL BE DISPLAY IF THE isPlaying IS true /-->
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from "./components/Block.vue"
 
 export default {
   name: 'App',
-  components: { }
+  components: { Block }, // DON'T FOGET TO REGISTER YOUR IMPORTED COMPONENTS HERE...
+
+  data() {
+    return {
+      isPlaying: false, // DETECT IF THE USER IS PLAYING...
+      delay: null,
+    }
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000 // GENERATING A RANDOM TIME DURATION FOR DELAY (MIN. TIME: 2 SEC | MAX. TIME: 7 SEC)
+      this.isPlaying = true
+    }
+  }
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="block" v-if="showBlock">
+      Hit me!
+  </div>
+</template>
+
+<script>
+export default {
+    props: [ "delay" ],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+
+    // VUE LIFECYCLE HOOK: MOUNTED
+    mounted() { // THIS IS HOW TO CALL HOOK LIFECYCLE
+        // TIMER WILL BE INITIALIZED HERE
+        setTimeout(() => {
+            this.showBlock = true
+        }, this.delay) // this.delay IS THE PASSED DATA IN PROPS
+    },
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0FAF87;
+        color: #FFF;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
