dd767c94a1725999278c833000408eda68ba112c
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-22 22:20:21 +0700

    Finishing touches was applied

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,33 @@
 <template>
-  <p>Reaction Time: {{ score / 1000 }} ms</p>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
     props: [ "score" ],
+
+    data() {
+        return {
+            rank: null
+        }
+    },
+
+    mounted() {
+        if (this.score < 250) return this.rank = "Excellent!"
+        else if (this.score < 400) return this.rank = "Ok, not bad"
+        else return this.rank = "Bruh, What a slow poke"
+    },
+
     methods: {
     }
 }
 </script>
 
 <style>
-
+    .rank {
+        font-size: 1.4em;
+        color: #0FAF87;
+        font-weight: bold;
+    }
 </style>
